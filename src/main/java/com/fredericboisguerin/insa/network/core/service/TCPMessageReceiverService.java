0227d47a2120f9dbb7798a7c38528210bf9655ca
moulinie <dorian.moulinie@gmail.com>
2017-11-29 10:41:12 +0100

    Making TCP works

--- src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageReceiverService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageReceiverService.java
@@ -1,8 +1,22 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TCPMessageReceiverService implements MessageReceiverService {
     @Override
     public void listenOnPort(int port, IncomingMessageListener incomingMessageListener) throws Exception {
-        throw new UnsupportedOperationException();
+        ServerSocket ss = new ServerSocket(port);
+
+        Socket s = ss.accept();
+        InputStreamReader isp = new InputStreamReader(s.getInputStream());
+
+        BufferedReader br = new BufferedReader(isp);
+        String msg = br.readLine();
+        incomingMessageListener.onNewIncomingMessage(msg);
+
+        ss.close();
     }
 }
--- src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageSenderService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageSenderService.java
@@ -1,8 +1,19 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class TCPMessageSenderService implements MessageSenderService {
     @Override
     public void sendMessageOn(String ipAddress, int port, String message) throws Exception {
-        throw new UnsupportedOperationException();
+        Socket ss = new Socket(ipAddress,port);
+
+        PrintWriter pr = new PrintWriter(ss.getOutputStream());
+
+        pr.print(message);
+
+        pr.close();
     }
 }

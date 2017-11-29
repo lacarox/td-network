1e413c7ea702883ee6b9565bf52e95b58eaa6d9f
moulinie <dorian.moulinie@gmail.com>
2017-11-29 10:30:00 +0100

    Making UDP works

--- src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageReceiverService.java
@@ -1,8 +1,23 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Socket;
+
 public class UDPMessageReceiverService implements MessageReceiverService {
     @Override
     public void listenOnPort(int port, IncomingMessageListener incomingMessageListener) throws Exception {
-        throw new UnsupportedOperationException();
+
+        DatagramSocket receiverSocket = new DatagramSocket(port);
+        DatagramPacket receiverPacket = new DatagramPacket(new byte[20],20);
+        receiverSocket.receive(receiverPacket);
+
+        byte[] data = receiverPacket.getData();
+        String msg = new String(data);
+
+
+        incomingMessageListener.onNewIncomingMessage(msg);
     }
 }
--- src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageSenderService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/UDPMessageSenderService.java
@@ -1,8 +1,24 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class UDPMessageSenderService implements MessageSenderService {
     @Override
     public void sendMessageOn(String ipAddress, int port, String message) throws Exception {
-        throw new UnsupportedOperationException();
+
+        DatagramSocket senderSocket = new DatagramSocket();
+
+        byte[] data = message.getBytes();
+
+        DatagramPacket senderPacket = new DatagramPacket(data, data.length);
+        senderPacket.setAddress(InetAddress.getByName(ipAddress));
+        senderPacket.setPort(port);
+        senderSocket.send(senderPacket);
+        senderSocket.close();
+
     }
 }
